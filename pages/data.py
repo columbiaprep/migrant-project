49d670ede31d48de46e3dd607b486d07fc4004a1
Nathan Rosenstrach <nrosenstrach@gmail.com>
2024-03-04 13:38:47 -0500

    hi

--- pages/data.py
+++ pages/data.py
@@ -7,4 +7,5 @@ def getLocations():
     url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.7128,-74.0060&radius=5000&keyword=homeless%20shelter&key=AIzaSyBudDAel4hrhurK0iGmhl9H3kMvzwZ3IqY'
     response = requests.request(url=url, method="GET")
     data = response.json()
+    print(data)
     return data
